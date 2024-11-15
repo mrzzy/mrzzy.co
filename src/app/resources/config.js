fd44620b603aabc3b67f9b44bffe2a6442d41f86
Zhu Zhanyan <program.nom@gmail.com>
2024-11-15 12:02:28 +0800

    refactor: disable blog & gallery features

--- src/app/resources/config.js
+++ src/app/resources/config.js
@@ -13,8 +13,8 @@ const routes = {
     '/':        true,
     '/about':   true,
     '/work':    true,
-    '/blog':    true,
-    '/gallery': true,
+    '/blog':    false,
+    '/gallery': false,
 }
 
 const effects = {
