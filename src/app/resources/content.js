1641ba81483284e63300a8e7121a885312b02f62
Zhu Zhanyan <program.nom@gmail.com>
2024-11-16 17:29:50 +0800

    chore: fix standard lint

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -103,45 +103,45 @@ const about = {
     title: 'Work Experience',
     experiences: [
       {
-        company: "FLY",
-        timeframe: "2022 - Present",
-        role: "Senior Design Engineer",
+        company: 'FLY',
+        timeframe: '2022 - Present',
+        role: 'Senior Design Engineer',
         achievements: [
-          <Fragment key="achieve_1">
+          <Fragment key='achieve_1'>
             Redesigned the UI/UX for the FLY platform, resulting in a 20%
             increase in user engagement and 30% faster load times.
           </Fragment>,
-          <Fragment  key="achieve_2">
+          <Fragment key='achieve_2'>
             Spearheaded the integration of AI tools into design workflows,
             enabling designers to iterate 50% faster.
-          </Fragment>,
+          </Fragment>
         ],
         images: [
           // optional: leave the array empty if you don't want to display images
           {
-            src: "/images/projects/project-01/cover-01.jpg",
-            alt: "Once UI Project",
+            src: '/images/projects/project-01/cover-01.jpg',
+            alt: 'Once UI Project',
             width: 16,
-            height: 9,
-          },
-        ],
+            height: 9
+          }
+        ]
       },
       {
-        company: "Creativ3",
-        timeframe: "2018 - 2022",
-        role: "Lead Designer",
+        company: 'Creativ3',
+        timeframe: '2018 - 2022',
+        role: 'Lead Designer',
         achievements: [
-          <>
+          <Fragment key='achieve_3'>
             Developed a design system that unified the brand across multiple
             platforms, improving design consistency by 40%.
-          </>,
-          <>
+          </Fragment>,
+          <Fragment key='achieve_4'>
             Led a cross-functional team to launch a new product line,
             contributing to a 15% increase in overall company revenue.
-          </>,
+          </Fragment>
         ],
-        images: [],
-      },
+        images: []
+      }
     ]
   },
   studies: {
