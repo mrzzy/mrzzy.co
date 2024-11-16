99b21fe5e11912b24b41e647af0721752fecf12c
Zhu Zhanyan <program.nom@gmail.com>
2024-11-16 16:32:34 +0800

    build: add eslint static analysis, standard code formatting

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,39 @@
+<<<<<<< HEAD
 import mdx from "@next/mdx";
+||||||| parent of 84b57c2 (build: add eslint static analysis, standard code formatting)
+import mdx from '@next/mdx';
+import createNextIntlPlugin from 'next-intl/plugin';
+=======
+import mdx from '@next/mdx'
+import createNextIntlPlugin from 'next-intl/plugin'
+>>>>>>> 84b57c2 (build: add eslint static analysis, standard code formatting)
 
 const withMDX = mdx({
+<<<<<<< HEAD
   extension: /\.mdx?$/,
   options: {},
 });
+||||||| parent of 84b57c2 (build: add eslint static analysis, standard code formatting)
+    extension: /\.mdx?$/,
+    options: { },
+});
+=======
+  extension: /\.mdx?$/,
+  options: { }
+})
+>>>>>>> 84b57c2 (build: add eslint static analysis, standard code formatting)
 
+<<<<<<< HEAD
+||||||| parent of 84b57c2 (build: add eslint static analysis, standard code formatting)
+const withNextIntl = createNextIntlPlugin();
+
+=======
+const withNextIntl = createNextIntlPlugin()
+
+>>>>>>> 84b57c2 (build: add eslint static analysis, standard code formatting)
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+<<<<<<< HEAD
   pageExtensions: ["ts", "tsx", "md", "mdx"],
   transpilePackages: ["next-mdx-remote"],
   images: {
@@ -23,5 +50,18 @@ const nextConfig = {
     silenceDeprecations: ["legacy-js-api"],
   },
 };
+||||||| parent of 84b57c2 (build: add eslint static analysis, standard code formatting)
+    pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+};
+=======
+  pageExtensions: ['ts', 'tsx', 'md', 'mdx']
+}
+>>>>>>> 84b57c2 (build: add eslint static analysis, standard code formatting)
 
+<<<<<<< HEAD
 export default withMDX(nextConfig);
+||||||| parent of 84b57c2 (build: add eslint static analysis, standard code formatting)
+export default withNextIntl(withMDX(nextConfig));
+=======
+export default withNextIntl(withMDX(nextConfig))
+>>>>>>> 84b57c2 (build: add eslint static analysis, standard code formatting)
