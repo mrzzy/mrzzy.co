638edf3cfd305223c45f9c59a311f4c520542744
Zhu Zhanyan <program.nom@gmail.com>
2025-12-26 07:56:26 +0000

    chore: format code with standard

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,27 @@
-import mdx from "@next/mdx";
+import mdx from '@next/mdx'
 
 const withMDX = mdx({
   extension: /\.mdx?$/,
-  options: {},
-});
+  options: {}
+})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ["ts", "tsx", "md", "mdx"],
-  transpilePackages: ["next-mdx-remote"],
+  pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
+  transpilePackages: ['next-mdx-remote'],
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "www.google.com",
-        pathname: "**",
-      },
-    ],
+        protocol: 'https',
+        hostname: 'www.google.com',
+        pathname: '**'
+      }
+    ]
   },
   sassOptions: {
-    compiler: "modern",
-    silenceDeprecations: ["legacy-js-api"],
-  },
-};
+    compiler: 'modern',
+    silenceDeprecations: ['legacy-js-api']
+  }
+}
 
-export default withMDX(nextConfig);
+export default withMDX(nextConfig)
